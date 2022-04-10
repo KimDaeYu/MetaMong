ae5c9e496c9f4b533e142bb99e115e10e363fa41
Park Changbeom <eric9710@naver.com>
2022-04-11 00:45:18 +0900

    Add firebase sdk
    Firebase auth, database, storage sdk were added.
    /Assets/Mapbox/core/Plugins/Android/support-compat-25.1.0.aar was
    disabled.

---
+++ Assets/Firebase/Editor/generate_xml_from_google_services_json.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:af702e89a736b9464ffa388046a5dc4194de1017f3cdaf9a517115243e7a8369
+size 17356
---
+++ Assets/Firebase/Editor/network_request.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f90e3741220d634256414c5879f339e660018b2d80e0806df8e4a0fc0c4e31f1
+size 12650
